380f1beb70eb723b980e3fe74467bc21276b42b2
Ning Sun <ningsunprc@gmail.com>
2019-09-06 00:08:21 +0800

    feat: remove debug information display, problem solved

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -141,8 +141,6 @@ class TLDetector(object):
         # return light_state
 
         # for testing
-        rospy.logdebug("traffic light state: %d", light.state)
-        rospy.logwarn("traffic light state: %d", light.state)
         return light.state
 
     def process_traffic_lights(self):
